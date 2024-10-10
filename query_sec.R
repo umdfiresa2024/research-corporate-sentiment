6fac9ba9eca9fc9134fa63fca59b7e2ec1166b05
Abhinav Akenapalli <aakena@terpmail.umd.edu>
2024-10-10 10:04:59 -0400

    getting tickers

--- query_sec.R
+++ query_sec.R
@@ -1,9 +1,34 @@
 library("httr")
+library("jsonlite")
+df<-read.csv("companylist.csv")
 
-name<-"1804"
-api<-"67f15f74f937e7f6376252601326b292d0002f7e51431616c2b3a5384329c981"
-url<-paste0("https://api.sec-api.io/mapping/name/", name, "?token=", api)
+comp<-df$x
 
-filename<-paste0("sec_api/", name, ".json")
-GET(url,write_disk(filename, overwrite = TRUE))
+tickers <- c()
+for (i in 3:length(comp)) {
+  name<- comp[i]
+  api<-"340127a6d808327065204d5164d913f36a7fb564ce3bf98362638b4c3da99d4c"
+  url<-paste0("https://api.sec-api.io/mapping/name/", name, "?token=", api)
+  
+  filename<-"sec_api/file.json"
+  GET(url,write_disk(filename, overwrite = TRUE))
+  
+  data <- fromJSON(paste0("sec_api/",name))
+  ticker <- data$ticker[1]
+}
+tickers
+#json_data <- fromJSON(paste(readLines('sec_api/1804.json'), collapse=""))
+#raw_json <- fromJSON(file = 1804.json)
+
+# convert json to a spreadsheet
+
+#json_df <- raw_json$cik
+
+#eia_df <- as.data.frame(do.call(rbind,   json_df ))
+
+#eia_df$respondent <- unlist(eia_df$respondent)
+
+#print(eia_df$respondent)
+
+
 
