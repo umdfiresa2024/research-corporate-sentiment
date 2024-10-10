e7d5dce51ffa08bd216be36df3d25496214f506d
ruangmas <ruangmas@umd.edu>
2024-10-10 12:48:05 -0400

    trycatch for json file

--- query_sec.R
+++ query_sec.R
@@ -1,11 +1,68 @@
 library("httr")
 library("jsonlite")
-df<-read.csv("companylist.csv")
+library("tidyverse")
+
+df<-read.csv("companylist.csv") %>%
+  mutate(x=str_remove(x, "LLC")) %>%
+  mutate(x=str_remove(x, "Inc.")) %>%
+  mutate(x=str_remove(x, "Inc")) %>%
+  mutate(x=str_remove(x, "INC")) %>%
+  mutate(x=str_remove(x, "LTD")) %>%
+  mutate(x=trimws(x, "both"))
 
 comp<-df$x
 
+
+get_sec <- function(i) {
+  tryCatch(
+    #try to do this
+    {
+      name<- comp[i]
+      api<-"340127a6d808327065204d5164d913f36a7fb564ce3bf98362638b4c3da99d4c"
+      url<-paste0("https://api.sec-api.io/mapping/name/", name, "?token=", api)
+      
+      filename<-"sec_api/file.json"
+      GET(url,write_disk(filename, overwrite = TRUE))
+      
+      data <- fromJSON("sec_api/file.json", simplifyVector = TRUE)
+      
+      eia_json_df <- raw_json$response$data
+      data2<-data %>%
+        filter(ticker!="")
+      
+      ticker <- data2$ticker[1]
+    
+      },
+    #if an error occurs, tell me the error
+    error=function(e) {
+      message('An Error Occurred')
+      print(e)
+    },
+    #if a warning occurs, tell me the warning
+    warning=function(w) {
+      message('A Warning Occurred')
+      print(w)
+      return(NA)
+    }
+  )
+}
+
+
+
 tickers <- c()
-for (i in 3:length(comp)) {
+#for (i in 2:length(comp)) {
+for (i in 2:5) {
+  print(i)
+  ticker <- get_sec(i)
+  tickers<-rbind(tickers, ticker)
+}
+  
+  
+  
+  
+  
+  
+  
   name<- comp[i]
   api<-"340127a6d808327065204d5164d913f36a7fb564ce3bf98362638b4c3da99d4c"
   url<-paste0("https://api.sec-api.io/mapping/name/", name, "?token=", api)
