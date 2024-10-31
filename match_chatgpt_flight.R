d38458ec5aa5a461e5e53a2c6e9add50df33990d
Abhinav Akenapalli <aakena@terpmail.umd.edu>
2024-10-31 13:36:42 -0400

    match names

--- match_chatgpt_flight.R
+++ match_chatgpt_flight.R
@@ -3,7 +3,7 @@ library("readxl")
 
 raw_flight<-read.csv("companylist.csv")
 
-chatgpt_output<-read_excel("G:/Shared drives/2024 FIRE-SA/Fall Course/tickers.xlsx")
+chatgpt_output<-read_excel("H:/Shared drives/2024 FIRE-SA/Fall Course/tickers.xlsx")
 chatgpt_output2<-read.csv("corporate sentiment - Sheet1.csv", header=FALSE)
 names(chatgpt_output2)<-c("company", "ticker")
 chatgpt_output3<-read.csv("company final tickers.csv")
@@ -11,27 +11,36 @@ names(chatgpt_output3)<-c("company", "ticker")
 
 chat<-rbind(chatgpt_output, chatgpt_output2, chatgpt_output3)
 
-flight.name = data.frame(raw_flight$x)
+library("stringi")
+
+flight.name<-as.data.frame(raw_flight$x)
 names(flight.name)<-"flight.name"
-chat.name = data.frame(chat$company)
-names(chat.name)<-"chat.name"
+
+chat.name <-stri_encode(chat$company, "","UTF-8")
 
 #flight=sp500
 #chat=nyse
-flight.name$chat.name <- "" # Creating an empty column
-
+flight_output <- c()
 #for(i in 2:dim(flight.name)[1]) {
-for (i in 2:600) {
+for (i in 2:100) {
   print(i)
-  x <- agrep(flight.name$flight.name[i], chat.name$chat.name,
-             ignore.case=TRUE, value=TRUE,
-             max.distance = 0.05, useBytes = TRUE)
-  x <- paste0(x,"")
-  flight.name$chat.name[i] <- x
+  
+  distmatrix <- stringdist::stringdistmatrix(flight.name[i,1], 
+                                             chat.name, 
+                                             method = 'lcs', p = 0.1)
+  
+  best_fit <- apply(distmatrix, 1, which.min)
+  similarity <- apply(distmatrix, 1, min)
+  output<-as.data.frame(cbind(flight.name[i,1], chat.name[best_fit], round(similarity,3)))
+  flight_output<-rbind(flight_output, output)
 }
 
 df<-read.csv("cleanedflight.csv")
 #merge flight.name to df
+merged <- merge(flight.name, df, by.x = "chat.name", by.y = "PARENT.COMPANY")
+grouped_merge <- merged %>% 
+  group_by(ticker, year) %>%
+  summarize(GHG=sum(GHG))
 
 #group_by(ticker, year) %>%
 #summarize(GHG=sum(GHG))
