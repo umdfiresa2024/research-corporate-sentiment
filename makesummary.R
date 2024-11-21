b4b00627a7c1305a7daaa536f10ce1f63bec6ca8
Thanicha Ruangmas <ruangmas@umd.edu>
2024-11-21 14:05:21 -0500

    add plots

--- makesummary.R
+++ makesummary.R
@@ -9,8 +9,31 @@ library("modelsummary")
 
 datasummary_skim(df)
 
-png("summary.png", res=500)
+####################################3
+df<-read.csv("finaldata.csv")
 
-datasummary(year + GHG + sentences + reduction + netzero + error ~ Mean + SD + Min + Max, fmt=0, data=df)
+df2<-df |>
+  mutate(score=(reduction+netzero)*100/sentences) |>
+  mutate(PARENT.COMPANY=ifelse(str_detect(PARENT.COMPANY, "Chevron"), "Chevron", PARENT.COMPANY))
+
+png("Poster Stuff/correlation.png", width=8, height=5, units="in", res=500)
+
+ggplot(df2, aes(x=GHG, y=score, col=PARENT.COMPANY)) + geom_point() + 
+  geom_smooth(method="lm", se=FALSE) +
+  theme_bw()
 
 dev.off()
+
+summary(m1<-lm(GHG ~ score + as.factor(ticker), data=df2))
+
+library("fixest")
+m1<-feols(GHG ~ score:as.factor(ticker) | ticker + year, data=df2)
+
+png("Poster Stuff/coefficients.png", width=8, height=5, units="in", res=500)
+
+modelplot(m1) +
+  labs(title="Correlation Between Score and GHG by Company", 
+       subtitle="After including year and company fixed effects")
+
+dev.off()
+       
